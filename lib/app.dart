0aa80710b9373dbaf2a60f7372dba009df6101c7
Ahmed Elsankary <elsankary99@gmail.com>
2023-10-22 02:06:40 +0300

    create app arctecther 2

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:green_taxi/view/pages/splash/splash_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: SplashPage(),
+    );
   }
 }
---
+++ lib/view/pages/otp/otp_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OTPPage extends StatelessWidget {
+  const OTPPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
