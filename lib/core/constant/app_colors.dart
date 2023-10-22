0020ffadeae980f41b2475a880373df9b44cc856
Ahmed Elsankary <elsankary99@gmail.com>
2023-10-22 19:57:21 +0300

    create app arctecther 3

---
+++ lib/core/constant/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xff303030);
+  static const Color green = Color(0xff2DBB54);
+  static const Color grey = Color(0xffD6D6D6);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color light = Color(0xffC3CDD6);
+}
---
+++ lib/core/constant/app_strings.dart
@@ -0,0 +1,14 @@
+class AppStrings {
+  static const kGoogleApiKey = "YOUR-Map-KEy",
+      helloNiceToMeetYou = 'Hello, nice to meet you!',
+      getMovingWithGreenTaxi = 'Get moving with Green Taxi',
+      enterMobileNumber = 'Enter your mobile number',
+      byCreating = 'By creating an account, you agree to our',
+      termsOfService = 'Terms of Service',
+      of = 'of',
+      privacyPolicy = 'Privacy Policy',
+      phoneVerification = 'Phone Verification',
+      enterOtp = 'Enter your OTP code below',
+      resendCode = 'Resend code in',
+      seconds = 'seconds';
+}
