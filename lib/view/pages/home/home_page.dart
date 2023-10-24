7454f69d6e4f7b8875389c8ee03071901a5c266b
Ahmed Elsankary <elsankary99@gmail.com>
2023-10-24 13:15:37 +0300

    change map style

--- lib/view/pages/home/home_page.dart
+++ lib/view/pages/home/home_page.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 @RoutePage()
@@ -13,40 +12,35 @@ class HomePage extends StatefulWidget {
 }
 
 class MapSampleState extends State<HomePage> {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
+  String? _mapStyle;
+
+  GoogleMapController? _controller;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  @override
+  void initState() {
+    super.initState();
+
+    rootBundle.loadString('assets/map_style.txt').then((string) {
+      _mapStyle = string;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        zoomControlsEnabled: false,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+          _controller = controller;
+          _controller!.setMapStyle(_mapStyle);
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
-      ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
